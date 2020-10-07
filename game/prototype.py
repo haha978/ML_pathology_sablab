0e07707074ff243a642785d289d52e1c89a3c410
Cagla Deniz Bahadir <cagladeniz94@gmail.com>
2020-10-07 12:55:06 -0400

    Add files via upload

--- game/prototype.py
+++ game/prototype.py
@@ -5,33 +5,75 @@ from pyglet.window import mouse
 import pyglet.shapes as shapes
 import random
 import time
+import argparse
+import csv
+import numpy as np
+import openslide
+from openslide import OpenSlideError
+from openslide.deepzoom import DeepZoomGenerator
 
-lib = { 'dog':[], 'cat':[] }
-tile_name_list = os.listdir('C:\\Users\\Joon Il Moon\\Desktop\\research\\code\\game\\images')
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('--path', type=str, default='/Users/caglabahadir/Desktop/Prototype/', help='Input path')
+parser.add_argument('--magnification',type=float, default=10)
+parser.add_argument('--tile_size',type=int, default=256)
+parser.add_argument('--output_path',type=str,default="/Users/caglabahadir/Desktop/Prototype/")
+
+args = parser.parse_args()
+#def main():
+
+with open(args.output_path+'names.csv', 'w') as csvfile:
+    fieldnames = ['Path', 'Magnification','Tile','Action','No Action']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+lib = { 'Action':[], 'No Action':[] }
+
+## Collecting the list of all svs files in a directory
+#tile_name_list =  os.listdir(args.path)
+tile_name_list =[]
+for root, dirs, files in os.walk(args.path):
+    for file in files:
+        if file.endswith('.svs'):
+                tile_name_list.append(root+"/"+file)
+
+## A placeholder to store number of action patches per svs defined by the pathologist
+action_patch = np.zeros(len(tile_name_list))
+
 #set game window
 game_window = pyglet.window.Window(1240, 800)
+
 #define cursor
 cursor = game_window.get_system_mouse_cursor(game_window.CURSOR_HAND)
 game_window.set_mouse_cursor(cursor)
+
 #define resource, which is where I pull the tiles
 st_time = time.time()
-pyglet.resource.path = ['C:\\Users\\Joon Il Moon\\Desktop\\research\\code\\game\\images']
+pyglet.resource.path = [args.path]
 pyglet.resource.reindex()
 end_time = time.time()
 print('Time took to make resource path',(end_time-st_time))
+
 #get center of an image
 def center_image(image):
      image.anchor_x = image.width // 2
      image.anchor_y = image.height // 2
-#all_tiles contain (tile_name,image)
 
+#all_tiles contain (tile_name,image)
 def get_all_tiles(list):
     all_tiles = []
     for tile_name in list:
         #this is a problem
         #try multi pooling -> order of addition really does not matter
         st_time = time.time()
-        im = pyglet.resource.image(tile_name)
+        #slide = openslide.open_slide(filepath)
+        slide = openslide.open_slide(tile_name)
+        generator = DeepZoomGenerator(slide, tile_size=224, overlap=0, limit_bounds=True)
+        ima = generator.get_tile(10, (0, 0))
+        ima.save("/Users/caglabahadir/Desktop/Prototype/tile_"+tile_name[-10:-4]+".png")
+        im = pyglet.resource.image("tile_"+tile_name[-10:-4]+".png")
         end_time = time.time()
         print('Time took to bring 1 image',(end_time-st_time))
         center_image(im)
@@ -60,7 +102,7 @@ level_label = pyglet.text.Label(text = "My Amazing Game",
         x=game_window.width//2, y=game_window.height-30,
         anchor_x='center', batch = main_batch, group=foreground)
 #create class label
-class_text = random.choice(list(lib.keys()))
+class_text = "Action"
 class_label = pyglet.text.Label(text = class_text,
             x=game_window.width//2, y=game_window.height-300,
             anchor_x='center', batch = main_batch, group=background)
@@ -90,9 +132,15 @@ sprite_i,sprite_j = draw_helper(tile_i,tile_j,image_batch)
 Initialize buttons and boxes available
 """
 next_box = shapes.Rectangle(x=game_window.width//2, y = 30, batch = main_batch, color =(100, 100, 255), width = 50, height=30, group = background)
+
+quit_box = shapes.Rectangle(x=3*game_window.width//4, y = 35, batch = main_batch, color =(100, 100, 255), width = 50, height=30, group = background)
+
 center_image(next_box)
 next_text = pyglet.text.Label(text = 'NEXT',x=game_window.width//2, y = 30,
         batch = main_batch, anchor_x = 'center',anchor_y = 'center', color = (255,255,255,255) ,group = foreground)
+
+quit_text = pyglet.text.Label(text = 'QUIT',x=3*game_window.width//4, y = 35,
+        batch = main_batch,  color = (255,255,255,255) ,group = foreground)
 
 button1 = shapes.Circle(x=413, y=150, radius = 15, batch = main_batch, group = background)
 button1_checked = shapes.Circle(x=413, y=150, radius = 12, batch = main_batch,
@@ -122,6 +170,13 @@ def in_box(x,y,x_center,y_center,x_width,y_height):
     bottom_y, top_y = (y_center - y_height/2), (y_center + y_height/2)
     return left_x < x and x < right_x and bottom_y < y and y < top_y
 
+def save_entry (path,tile,action,no_action):
+    with open(args.output_path + 'names.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Path', 'Magnification', 'Tile', 'Action', 'No Action']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        # writer.writeheader()
+        writer.writerow({'Path': path, "Magnification": args.magnification,
+                         "Tile": tile, "Action": action, "No Action": no_action})
 @game_window.event
 def on_mouse_press(x, y, button, modifiers):
     global sprite_i, sprite_j, tile_name_i, tile_name_j,tile_i,tile_j,all_tiles_len
@@ -141,16 +196,25 @@ def on_mouse_press(x, y, button, modifiers):
     elif in_box(x,y,game_window.width//2,30,50,30):
         print('I am clicking next block')
         #save the result
-        if class_text == 'dog':
+        path="hey"
+        path_2="hey"
+        tile="my"
+        tile_2="my_2"
+        if class_text == 'Action':
             if button1_checked.color == [0,0,0]:
-                lib['dog'].append(tile_name_i)
+                lib['Action'].append(tile_name_i)
+                save_entry(path,tile,"true","false")
+            else:
+                lib['No Action'].append(tile_name_i)
+                save_entry(path, tile, "false", "true")
             if button2_checked.color == [0,0,0]:
-                lib['dog'].append(tile_name_j)
-        if class_text == 'cat':
-            if button1_checked.color == [0,0,0]:
-                lib['cat'].append(tile_name_i)
-            if button2_checked.color == [0,0,0]:
-                lib['cat'].append(tile_name_j)
+                lib['Action'].append(tile_name_j)
+                save_entry(path_2, tile_2, "true", "false")
+            else:
+                lib['No Action'].append(tile_name_j)
+                save_entry(path_2, tile_2, "false", "true")
+
+
         #reset buttons
         button1_checked.color = [255,255,255]
         button2_checked.color = [255,255,255]
@@ -159,7 +223,7 @@ def on_mouse_press(x, y, button, modifiers):
         print(lib)
         #set a new_text box
         class_label.delete()
-        class_text = random.choice(list(lib.keys()))
+        class_text = "Action"
         class_label = pyglet.text.Label(text = class_text,
                     x=game_window.width//2, y=game_window.height-300,
                     anchor_x='center', batch = main_batch, group=foreground)
